e7f40eb41226f7682c77154cc0ddbc30b098c544
mukesh <billamukesh1234@gmail.com>
2024-08-03 00:05:48 +0530

    Time: 5 ms (71.28%), Space: 44.3 MB (11.19%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,14 +2,18 @@ class Solution {
     public boolean isPalindrome(int x) {
         int dup = x;
         int reverse = 0;
+
         while(x > 0){
             int rem = x % 10;
             x /= 10;
             reverse = reverse * 10 + rem;
         }
-        if(reverse != dup){
-            return false;
-        }
-        return true;
+
+        // if(reverse != dup){
+        //     return false;
+        // }
+        return reverse != dup ? false : true;
+
+        // return true;
     }
 }
