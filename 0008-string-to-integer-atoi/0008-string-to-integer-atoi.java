aa8a849be03d106e1f62cc50784d1ac9466502d0
Mukesh Billa <billamukesh1234@gmail.com>
2025-01-23 21:28:54 +0530

    Time: 2 ms (30.55%), Space: 42.3 MB (78.93%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if(s.isEmpty()) return 0;
+
+        int sign = 1, index = 0;
+
+        if (s.charAt(0) == '-') {
+            sign = -1;
+            index++;
+        } else if (s.charAt(0) == '+') index++;
+
+        long num = 0;
+        for(int i = index; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(!Character.isDigit(c)) break;
+
+            num = num * 10 + (c - '0');
+
+            if (num * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (num * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        }
+
+        return (int) num * sign;
+    }
+}
