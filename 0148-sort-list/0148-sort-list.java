3e8b3ee832af9054c1ad66477e4e3e062d1ccbb6
Mukesh Billa <billamukesh1234@gmail.com>
2025-05-28 20:06:40 +0530

    Time: 9 ms (96.03%), Space: 56.9 MB (56.13%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,25 +9,54 @@
  * }
  */
 class Solution {
+    public ListNode mergeTwoSortedLinkedLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        if(list1 != null){
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode findMid(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
     public ListNode sortList(ListNode head) {
-        List<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
+        if(head == null || head.next == null) return head;
 
-        while(temp != null){
-            arr.add(temp.val);
-            temp = temp.next;
-        }
+        ListNode mid = findMid(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+        
+        left = sortList(left);
+        right = sortList(right);
 
-        Collections.sort(arr);
-
-        int i = 0;
-        temp = head;
-        while(temp != null){
-            temp.val = arr.get(i);
-            temp = temp.next;
-            i++;
-        }
-
-        return head;
+        return mergeTwoSortedLinkedLists(left, right);
     }
 }
