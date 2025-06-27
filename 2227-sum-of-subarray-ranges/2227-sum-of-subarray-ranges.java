7469f83d69ebd781914a2ddcb6b81d9b1cd430cd
Mukesh Billa <billamukesh1234@gmail.com>
2025-06-27 23:26:07 +0530

    Time: 15 ms (96.02%), Space: 44.2 MB (80.64%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,16 +1,28 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
+        return getSubarraySum(nums, true) - getSubarraySum(nums, false);
+    }
+
+    private long getSubarraySum(int[] nums, boolean isMax) {
         int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
         long sum = 0;
-        for(int i = 0; i < n; i++){
-            long largest = nums[i];
-            long smallest = nums[i];
-            for(int j = i + 1; j < n; j++){
-                largest = Math.max(largest, nums[j]);
-                smallest = Math.min(smallest, nums[j]);
-                sum = sum + (largest - smallest);
-            }     
+
+        for (int i = 0; i <= n; i++) {
+            while (!stack.isEmpty() && (i == n || 
+                (isMax && nums[stack.peek()] < (i < n ? nums[i] : Integer.MAX_VALUE)) || 
+                (!isMax && nums[stack.peek()] > (i < n ? nums[i] : Integer.MIN_VALUE)))) {
+
+                int mid = stack.pop();
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                int right = i;
+
+                long count = (mid - left) * (right - mid);
+                sum += count * nums[mid];
+            }
+            stack.push(i);
         }
+
         return sum;
     }
 }
